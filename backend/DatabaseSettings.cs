4a5c35d24aad311a220956b66741676e6aee69e0
Joe Hadchity <hadchityjoe64@gmail.com>
2024-09-02 21:32:15 +0300

    Initial commit: Setup .NET project with MongoDB configuration

---
+++ backend/DatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace backend
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; } = String.Empty;
+        public string DatabaseName { get; set; } = String.Empty;
+    }
+}
