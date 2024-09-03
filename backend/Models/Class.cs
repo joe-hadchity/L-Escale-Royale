18d78b0addda5b51a308e86951344c98108b15a6
AnthonyBadr <anthonybadr40@gmail.com>
2024-09-03 15:36:03 +0300

    new changes trial

---
+++ backend/Models/Class.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string email { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+
+
+    }
+}
