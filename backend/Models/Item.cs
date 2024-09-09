388022e872a72c0e8dc28ff2108f5bef3cb650e9
AnthonyBadr <anthonybadr40@gmail.com>
2024-09-09 09:49:03 +0300

    dfgdfg

---
+++ backend/Models/Item.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace backend.Models
+{
+    public class Item
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string categoryId {  get; set; }
+
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+
+        public double price { get; set; }
+
+
+
+    }
+}
